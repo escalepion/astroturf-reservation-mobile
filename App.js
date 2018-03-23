38afa7d2c498a5e921b101fe8c16164953a81a4a
Akif Mucurluoglu <akifmucurlu@gmail.com>
2018-03-23 21:39:36 +0300

    add navigation with authentication

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
+import { createRootNavigator } from './src/navigation/routes';
 
 import reducers from './src/reducers';
 
@@ -9,10 +10,12 @@ import Index from './src/screens/Index';
 
 export default class App extends React.Component {
   render() {
+    const userLogged = true;
+    const RootNavigator = createRootNavigator(userLogged);
     const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
     return (
       <Provider store={createStoreWithMiddleware(reducers)}>
-        <Index />
+        <RootNavigator />
       </Provider>
     );
   }
