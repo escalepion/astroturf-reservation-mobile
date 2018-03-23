2f13efac4040a79b1ba2387e7664ee6e24fa0bbe
Akif Mucurluoglu <akifmucurlu@gmail.com>
2018-03-23 20:51:02 +0300

    create screens

--- App.js
+++ App.js
@@ -1,30 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
 
 import reducers from './src/reducers';
 
+import Index from './src/screens/Index';
+
 export default class App extends React.Component {
   render() {
     const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
     return (
       <Provider store={createStoreWithMiddleware(reducers)}>
-        <View style={styles.container}>
-          <Text>Open up App.js to start working on your app!</Text>
-        </View>
+        <Index />
       </Provider>
     );
   }
 }
 
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
