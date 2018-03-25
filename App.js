de8f3b700ee2634f98691d009a62798da4ee92d1
Akif Mucurluoglu <akifmucurlu@gmail.com>
2018-03-25 14:23:45 +0300

    add firebase

--- App.js
+++ App.js
@@ -3,11 +3,23 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
 import { createRootNavigator } from './src/navigation/routes';
+import firebase from 'firebase';
 
 import reducers from './src/reducers';
 
 
 export default class App extends React.Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyBTAyusc-Qa7zmI2HWvMn17SlRh3WU8L_U',
+      authDomain: 'astruturf.firebaseapp.com',
+      databaseURL: 'https://astruturf.firebaseio.com',
+      projectId: 'astruturf',
+      storageBucket: 'astruturf.appspot.com',
+      messagingSenderId: '847295546379'
+    };
+    firebase.initializeApp(config);
+  }
   render() {
     const userLogged = true;
     const RootNavigator = createRootNavigator(userLogged);
