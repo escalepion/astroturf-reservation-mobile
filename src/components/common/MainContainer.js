9c5f75858d25c408bccb808c3fc802f5b0f57136
Akif Mucurluoglu <akifmucurlu@gmail.com>
2018-03-25 11:17:34 +0300

    main container

---
+++ src/components/common/MainContainer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const MainContainer = (props) => {
+    return <View style={styles.container}>{props.children}</View>;
+};
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#FFFFFF',
+        borderWidth: 0.5,
+        borderColor: '#d6d7da',
+        display: 'flex',
+        marginTop: 5
+    }
+});
+
+export default MainContainer;
--- src/components/index/IndexHeader.js
+++ src/components/index/IndexHeader.js
@@ -2,30 +2,29 @@ import React from 'react';
 import { View, StyleSheet, TouchableOpacity } from 'react-native';
 import { Icon } from 'react-native-elements';
 
+import MainContainer from '../common/MainContainer';
+
 const IndexHeader = () => {
     return (
-        <View style={styles.container}>
-            <TouchableOpacity>
-                <Icon
-                    raised
-                    name="add"
-                    color='#FFFFFF'
-                    containerStyle={{ backgroundColor: '#6733BA' }}
-                />
-            </TouchableOpacity>
-        </View>
+        <MainContainer>
+            <View style={styles.container}>
+                <TouchableOpacity>
+                    <Icon
+                        raised
+                        name="add"
+                        color='#FFFFFF'
+                        containerStyle={{ backgroundColor: '#6733BA' }}
+                    />
+                </TouchableOpacity>
+            </View>
+        </MainContainer>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: '#FFFFFF',
-        borderWidth: 0.5,
-        borderColor: '#d6d7da',
         display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 5
+        alignItems: 'center'
     }
 });
 
