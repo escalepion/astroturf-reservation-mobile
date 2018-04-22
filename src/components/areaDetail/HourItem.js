73d97f5153358a6384abbc14d07365a7035605f5
Akif Mucurluoglu <akifmucurlu@gmail.com>
2018-04-22 12:19:57 +0300

    styling hour item

--- src/components/areaDetail/HourItem.js
+++ src/components/areaDetail/HourItem.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { Icon } from 'react-native-elements';
 
 const HourItem = ({ index, HoursMap, onAddPress }) => {
+    const { hourItemContainer } = styles;
     return (
-        <View>
+        <View style={hourItemContainer}>
             <Text>{`${HoursMap[index]} - ${HoursMap[index + 1]}`}</Text>
-            <Icon name="add" color='#FFFFFF' containerStyle={{ backgroundColor: '#6733BA' }} onPress={onAddPress} />
+            <Icon raised name="add" color='#FFFFFF' containerStyle={{ backgroundColor: '#6733BA' }} onPress={onAddPress} />
         </View>
     );
 };
 
 export default HourItem;
+
+const styles = StyleSheet.create ({
+    hourItemContainer : {
+        backgroundColor: '#dddddd',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginTop: 2,
+        paddingRight: 5,
+        paddingLeft: 5
+    }
+});
